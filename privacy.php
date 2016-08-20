e595474a1b2c87c283da3dac30ebd5dedf31431d
Pratik Solim <pratik.solim@gmail.com>
2016-08-20 17:43:58 +0530

    Added privacy policy

---
+++ privacy.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Privacy policy</title>
+</head>
+<body>
+  <ul>
+    <li>Arceus is Facebook Messenger chatbot, which replies to user queries using Wolfram | Alpha API.</li>
+    <li>Arceus does not save any user information in its database.</li>
+    <li>Only chat history will remain in account</li>
+  </ul>
+</body>
+</html>
